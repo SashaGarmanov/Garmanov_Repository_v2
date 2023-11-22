c0c51adac47de8b9b165daae1522857f412a9cc6
SashaGarmanov <sagagarm8357@gmail.com>
2023-11-22 11:09:47 +0300

    new commit

--- main.c
+++ main.c
@@ -1,23 +1,21 @@
-/* main.c */
-#include <stdio.h>
-#include "askname.h"
-int main(int argc, char **argv)
-{
- char first[255], last[255];
- askname(first, last);
- printf("Hello, %s %s!\n", first, last);
- return 0;
-}
-/* askname.c */
 #include <stdio.h>
 #include <string.h>
-void askname(char *first, char *last)
+int main(int argc, char **argv)
 {
+<<<<<<< HEAD
+ char name[255];
+ fgets(name, 255, stdin);
+ printf("length= %d\n", strlen(name)); /* debug line */
+ name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+ printf("Hello", name);
+=======
+ char first[255], last[255];
  printf("Enter your first name: ");
  fgets(first, 255, stdin);
  first[strlen(first)-1] = '\0'; /* remove the newline at the end */
  printf("Now enter your last name: ");
  gets(last); /* buffer overflow? what's that? */
-}
-/* askname.h */
-void askname(char *first, char *last);
+ printf("Hello,", first, last);
+>>>>>>> e3443ccb7d3e550dcb52bd23d27a8b151afbbc4b
+ return 0;
+}
